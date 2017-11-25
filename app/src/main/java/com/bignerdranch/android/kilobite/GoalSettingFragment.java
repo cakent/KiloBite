8f09baa5611e635cd4b91e748d6eea9970ad213b
Cakent <cakent@memphis.edu>
2017-11-25 10:07:30 -0600

    Added Menus, Workout will current update the reps of two filler workouts in response to calories needed to burn.

--- app/src/main/java/com/bignerdranch/android/kilobite/GoalSettingFragment.java
+++ app/src/main/java/com/bignerdranch/android/kilobite/GoalSettingFragment.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 
 import com.bignerdranch.android.kilobite.database.WorkoutBaseHelper;
 
+import java.util.List;
+
 /**
  * Created by kent on 11/10/17.
  */
@@ -27,6 +29,8 @@ public class GoalSettingFragment extends Fragment {
 
     private Button mWorkoutButton;
     private User mUser;
+    private List<Workout> mWorkouts;
+
     private static final String TAG ="GoalSettingFragment";
 
 
@@ -79,13 +83,26 @@ public class GoalSettingFragment extends Fragment {
 
 
 
-
         mWorkoutButton = (Button) v.findViewById(R.id.getWODSButton);
         mWorkoutButton.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(getActivity(),WorkoutListActivity.class);
-                startActivity(intent);
+
+                if(mUser.getCurrentHeight()!=0&&mUser.getCurrentWeight()!=0) {
+                   int userWeight =mUser.getCurrentWeight();
+                    int userHeight =mUser.getCurrentHeight();
+
+                    double BMI = CalculateBMI(userWeight,userHeight);
+                    mUser.setBMI(BMI);
+                    double goalWeight = toGetInRange(userWeight,userHeight,BMI);
+                    mUser.setGoalWeight(goalWeight);
+                    for(int i =0;i<mWorkouts.size();i++){
+                    updateWorkoutReps(goalWeight,i);}
+                    Intent intent = new Intent(getActivity(), WorkoutListActivity.class);
+                    startActivity(intent);
+                }
+
+
             }
         });
 
@@ -102,18 +119,57 @@ public class GoalSettingFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         int userID = (int) getActivity().getIntent().getSerializableExtra(GoalSettingActivity.EXTRA_USER_ID);
+        mWorkouts=WorkoutLab.get(getActivity()).getWorkouts();
         mUser=UserLab.get(getActivity()).getUser(userID);
+
 
     }
     @Override
     public void onPause(){
         super.onPause();
         UserLab.get(getActivity()).updateUser(mUser);
+
     }
 
     private void updateUser(){
         UserLab.get(getActivity()).updateUser(mUser);
         Log.d(TAG,"Updating the user");
     }
+
+    private void updateWorkout(Workout w){
+        WorkoutLab.get(getActivity()).updateWorkout(w);
+        Log.d(TAG,"Updating the workout");
+    }
+
+    private void updateWorkoutReps(double goalCalBurned, int index){
+        Workout baseworkout = mWorkouts.get(index);
+        int baseNum = baseworkout.getWorkoutNum();
+        int ratio = (int) (goalCalBurned/18)/baseNum;
+        int[] newReps = baseworkout.getReps();
+        for(int i =0;i<newReps.length;i++){
+            newReps[i] = newReps[i]*ratio;
+        }
+        baseworkout.setReps(newReps);
+        updateWorkout(baseworkout);
+    }
+
+    public double CalculateBMI(int userWeight, int userHeight){
+
+        double height=userHeight;
+        double weight=userWeight;
+        double BMI;
+        BMI =(weight)/(height*height)*703;
+
+        return BMI;
+    }
+
+    public double toGetInRange(int UserWeight, int userHeight, double userBMI){
+        double difference = userBMI-24.9;
+
+        double BMItoPounds = (userHeight*0.0254)*(userHeight*0.0254)/0.453592;
+        double toGetInRange = difference*BMItoPounds;
+        return toGetInRange*3500;
+    }
+
 
 }
